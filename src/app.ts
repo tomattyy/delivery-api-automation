f51c26510a3ae3c53816caf62bfdd9da43aaa178
tomattyy <oskamattheus.dev@gmail.com>
2026-07-03 16:32:31 -0300

    teste4

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import orderRoutes from './routes/orderRoutes';
-app = express();
+
+const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/orders', orderRoutes);
+app.use('/api/order', orderRoutes);
 
 export default app;
